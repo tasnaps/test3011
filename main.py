9a185ce084b9f10026aa7b721a7700c2500ed133
Tapio Meriläinen <tapiom4@gmail.com>
2022-12-07 12:14:15 +0200

    not working

--- main.py
+++ main.py
@@ -8,116 +8,184 @@ from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 from pybricks.iodevices import Ev3devSensor
 from pybricks.nxtdevices import LightSensor
+# micropython module umath not pybricks module
+import math as math
 ev3 = EV3Brick()
 
 # Wheels on b and c for correct lego calibration
 left_motor = Motor(Port.B)
 right_motor = Motor(Port.C)
-base = DriveBase(left_motor, right_motor, 60, 170)
+
+# set third argument according to robot
+# 170 for small wheel robot
+#55,  120 for big wheel robot
+base = DriveBase(left_motor, right_motor, 55, 120)
 left_ir_sensor = Ev3devSensor(Port.S1)
 right_ir_sensor = Ev3devSensor(Port.S2)
-left_ir_sensor_value = left_ir_sensor.read("DC")
-right_ir_sensor_value = right_ir_sensor.read("DC")
-
-# when we set the robot in arena, must face forward we update this in degrees turned
-
+# when we set the robot in arena, must face forward we update heading in degrees turned
 heading = 0
 locationToLine = 0
 rotation = 0
+global angle
+
 
 def deduct(value):
-    returnable = value
-    if value>1:
-        returnable =-1
-        deduct(returnable)
-    if value<-1:
-        returnable =+1
-        deduct(returnable)
+    if value > 1:
+        value -= 1
+        deduct(value)
+    if value < -1:
+        value +=1
+        deduct(value)
+    return value
 
-# Problem might be in this function
+
 def getRotation():
-    #Cleaning data
+    global heading
     newHeading = heading/360
-    deduct(newHeading)
-    #newHeading = newHeading * 360
-    print("heading is: " + str(newHeading))
-    return abs(newHeading)
+    newRotation = deduct(newHeading)
+    #return angle in decimals
+    return newHeading
 
-
-
+#should be ok. TODO needs testing
 def updateLocation():
-    tempHeading = getRotation()
-    #Update on the location based on which side we have moved. 
-    if tempHeading<0.5:
+    global locationToLine
+    angle = getRotation()
+
+    #positive = right side of the arena
+    if (angle < 0.5 and angle >0.0):
         locationToLine += 150
-    if tempHeading>0.5:
+    if (angle >-1.0 and angle<-0.5):
+        locationToLine +=150
+
+    #negative = left side of the arena
+    if(angle<1 and angle >0.5):
+        locationToLine -=150
+    if(angle<0 and angle>-0.5):
         locationToLine -= 150
-    
 
-        
 def moveToLine():
-    tempHeading = getRotation() * 360
+    global rotation
+    global degreesToLine
+    tempHeading = getRotation() #value from -1 to 1
+    if (tempHeading < 0):
+        rotation = 1 - abs(tempHeading)
+    else:
+        rotation = 1 + tempHeading
 
-    #we are on the left side of the line
-    rotation = -tempHeading + 90
-    if locationToLine<0:
-        base.turn(rotation)
+    degreesToLine = (360 * rotation)-360    
+
+    if locationToLine < 0:
+        print("we are on the left side")
+        if(degreesToLine<0):
+            turn = abs(degreesToLine)
+            base.turn(turn + 90)
+        if(degreesToLine>0):
+            turn = -degreesToLine
+            base.turn(turn + 90)
         readAndMove()
         base.turn(-90)
-        base.straight(10000)
-    
-    #we are on the right side of the line
-    if locationToLine>0:
-        base.turn(rotation+180)
+        ballControl = checkBallControl()
+        if (ballControl):
+            base.straight(500)
+        else:
+            movement()
+
+    if locationToLine > 0:
+        print("We are on the right side of the area")
+        if(degreesToLine<0):
+            print("degreestoLine when <0")
+            print(degreesToLine)
+            base.turn(degreesToLine - 90)
+        if(degreesToLine>0):
+            degreesToLine = -degreesToLine - 90
+            print("degreesTo line when >0")
+            print(degreesToLine)
+            base.turn(degreesToLine)
         readAndMove()
         base.turn(90)
-        base.straight(10000)
+        ballControl = checkBallControl()
+        if (ballControl):
+            base.straight(500)
+        else:
+            movement()
 
-    #read the third sensor value and move forward until we hit the black line
-    #once we hit the line we turn depending on approach and move forward.
+def checkBallControl():
+    left = left_ir_sensor.read("DC")
+    right = right_ir_sensor.read("DC")
+    if left[0] > 7 and right[0] < 4:
+        # we still have the ball
+        return True
+    else:
+        return True
 
-#helper method for recursion
+def lineFollow():
+    leftSensor = ColorSensor(Port.S3)
+    rightSensor = ColorSensor(Port.S4)
+    leftValue = leftSensor.reflection()
+    rightValue = rightSensor.reflection()
+    if(rightValue<3):
+        base.turn(10)
+    if(leftValue<3):
+        base.turn(-9)
+    else:#(leftValue>3 and rightValue>3)
+        base.straight(100)
+    lineFollow()
+
+
 def readAndMove():
-    #TODO Need to check what value we get on black line 
+    #left color sensor
     color_sensor = ColorSensor(Port.S3)
     color_sensor_value = color_sensor.reflection()
-    print(color_sensor_value)
-    if(color_sensor_value > 3):
-        base.straight(10)
+    ballControl = checkBallControl()
+    if (color_sensor_value > 3 and ballControl): #and ballControl
+        base.straight(20)
         readAndMove()
+    if(ballControl == False):
+        movement()
 
 
-# one option is to only use one sensor, if we install it on the middle. Then the values should be ok when ball is in the middle.
-def movement(left_ir_sensor, right_ir_sensor):
+# works ok
+def movement():
     left_ir_sensor_value = left_ir_sensor.read("DC")
     right_ir_sensor_value = right_ir_sensor.read("DC")
+    global heading
 
-    if(right_ir_sensor_value[0] == 0 and left_ir_sensor_value[0] == 0):
-        print("ball behind")
-        base.turn(180)
-        heading =+180
-        movement(left_ir_sensor, right_ir_sensor)
+    # ball is behind the robot
+    if (right_ir_sensor_value[0] == 0 and left_ir_sensor_value[0] == 0):
+        base.turn(40)
+        heading += 40
+        movement()
 
-    if right_ir_sensor_value[0]>5:#6
-        print("ball slightly to the right")
+    # ball is to the right
+    if right_ir_sensor_value[0] > 5:  # 60
         base.turn(20)
-        heading =+20
-        movement(left_ir_sensor, right_ir_sensor)
+        heading += 20
+        movement()
 
-    if left_ir_sensor_value[0] < 5:#4
-        print("ball slightly to the left")
+    # ball is to the left
+    if left_ir_sensor_value[0] < 5:  # 4
         base.turn(-25)
-        heading =-25
-        movement(left_ir_sensor, right_ir_sensor)
-    
-    if left_ir_sensor_value[0]>7 and right_ir_sensor_value[0]<4:
-        print("ball between sensors")
-        # Call move to line function
+        heading -= 25
+        movement()
+
+    # ball between sensors
+    if left_ir_sensor_value[0] > 7 and right_ir_sensor_value[0] < 4:
         moveToLine()
-        
+
     else:
         base.straight(150)
         updateLocation()
-        movement(left_ir_sensor, right_ir_sensor)
+        movement()
 
-movement(left_ir_sensor, right_ir_sensor)
+
+# movement()
+#base.straight(250)
+#base.straight(-250)
+#base.turn(-150)
+#heading = -150
+#base.straight(150)
+#updateLocation()
+#moveToLine()
+movement()
+#base.turn(-130)
+#lineFollow()
